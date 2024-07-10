94e5a17fcf90d2f55b2173c7abc092bf56a696d4
aosenit <creativitytraz22@gmail.com>
2024-07-10 22:35:03 +0100

    first commit

---
+++ lib/auth/src/lib/auth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-auth',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './auth.component.html',
+  styles: ``,
+})
+export class AuthComponent {}
